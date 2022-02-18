e1234274ec9a4c43df943201b0f1c5e75e2d8d00
Tony <tonyperiod@gmail.com>
2022-02-18 22:08:25 +0000

    moving cards works
    floting cards no work yet, connected everything in scene

---
+++ Tarot/Assets/Scripts/Tony/DragTableProjection.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTableProjection : MonoBehaviour
+{
+    public Vector3 currentMousePosition;
+    private Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+   
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject.layer != LayerMask.NameToLayer("Table")) continue;
+
+            //Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.red);
+
+            currentMousePosition = hit.point;
+
+            break;
+        }
+    }
+}
