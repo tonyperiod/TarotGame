e56076d3331da5a16318c844ec8d3715e2c2591c
connerjharper444 <49066313+connerjharper444@users.noreply.github.com>
2022-03-16 18:32:24 +0000

    Tweaked card UI slightly to make cards fit better.

--- Tarot/Assets/Scripts/Conner/UIManager.cs
+++ Tarot/Assets/Scripts/Conner/UIManager.cs
@@ -12,8 +12,6 @@ public class UIManager : MonoBehaviour
     int cardDamage;
     int cardShield;
 
-    GameObject symbol;
-
 
     // Start is called before the first frame update
     void Start()
