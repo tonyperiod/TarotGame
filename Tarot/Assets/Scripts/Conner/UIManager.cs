d531e9a89ae3e4679aed83fbdcef3ea12645f863
connerjharper444 <49066313+connerjharper444@users.noreply.github.com>
2022-03-17 00:00:14 +0000

    Re-adding script to the project.

--- Tarot/Assets/Scripts/Conner/UIManager.cs
+++ Tarot/Assets/Scripts/Conner/UIManager.cs
@@ -7,7 +7,7 @@ public class UIManager : MonoBehaviour
 {
 
     GameObject gameObject;
-    public GameObject text; 
+    public GameObject text;
 
     int cardDamage;
     int cardShield;
@@ -16,25 +16,29 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       gameObject = GameObject.Find("Tooltip");
-       text.SetActive(false);
+        gameObject = GameObject.Find("Tooltip");
+        text.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnMouseEnter()
+    private void OnMouseOver()
     {
-        gameObject.SetActive(true);
-        gameObject.transform.position = this.transform.position + new Vector3(1, 0.7f, 2);
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Debug.Log("Right Mouse Clicked");
+            gameObject.SetActive(true);
+            gameObject.transform.position = this.transform.position + new Vector3(-1, 1f, 2);
+            text.SetActive(true);
+        }
 
-        text.SetActive(true);
-        
+
     }
-    
+
     public void UpdateCard(int damage, int shield)
     {
         cardDamage = damage;
@@ -45,8 +49,6 @@ public class UIManager : MonoBehaviour
     {
         gameObject.SetActive(false);
         text.SetActive(false);
-     
-
     }
 
 }
