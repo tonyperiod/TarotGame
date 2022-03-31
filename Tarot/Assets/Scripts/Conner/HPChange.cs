8648320cfa0d2477a0d5a11a1fb1f8a4b07d1803
connerjharper444 <49066313+connerjharper444@users.noreply.github.com>
2022-03-31 11:53:10 +0100

    Added HPSystem script so I can use to change numerical values.

--- Tarot/Assets/Scripts/Conner/HPChange.cs
+++ Tarot/Assets/Scripts/Conner/HPChange.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class HPChange : MonoBehaviour
 {
+    GameObject hptotaltext; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject hptotaltext = GameObject.Find("PlayerHPText");
+        //GameObject hptotaltext = GameObject.Find("PlayerHPText");
 
-        hptotaltext.transform.GetComponent<Text>().text = "/" + hpsystem.getHP().ToString();
+        //hptotaltext.transform.GetComponent<Text>().text = "/" + hpsystem.getHP().ToString();
     }
 }
