9e83ef52b8af5cc1f62a6d8efcc19321bd2af783
connerjharper444 <49066313+connerjharper444@users.noreply.github.com>
2022-04-08 16:58:14 +0100

    Added a conversation script to potentially add dialogue when you walk into the shop and added placeholder tooltip (for now).

--- Tarot/Assets/Scripts/Conner/ShopUIManager.cs
+++ Tarot/Assets/Scripts/Conner/ShopUIManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopUIManager : MonoBehaviour
 {
+    public GameObject gameObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,27 @@ public class ShopUIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Debug.Log("Right Mouse Clicked");
+            gameObject.SetActive(true);
+            gameObject.transform.position = new Vector3(950f, 500f, 0f);
+
+            gameObject.transform.GetChild(0).transform.GetComponent<Text>().text = this.GetComponent<ShopCardScriptReference>().elem + " Card";
+            gameObject.transform.GetChild(1).transform.GetComponent<Text>().text = this.GetComponent<ShopCardScriptReference>().value.ToString();
+
+        }
+
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.SetActive(false);
         
     }
 }
