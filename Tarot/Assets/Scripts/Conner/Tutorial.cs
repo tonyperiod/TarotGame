e1fbf685971e0c58767f0938e12e682cb990b868
connerjharper444 <49066313+connerjharper444@users.noreply.github.com>
2022-04-25 14:54:31 +0100

    Added script back into the game project.

---
+++ Tarot/Assets/Scripts/Conner/Tutorial.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    Transform transform;
+    GameObject gameObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       if (InterScene.currentScene == "TonyCardTesting")
+       {
+
+       }
+       else
+       {
+
+       }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SkipTutorial()
+    {
+       
+        
+    }
+
+    public void NextScreen()
+    {
+        
+    }
+}
