c6883c56ce0e57f0de588638a824f3f4334bc40d
SaltyFingerz <denis.iannoukos@yandex.com>
2022-05-03 01:30:33 +0100

    double projectile premature collision bug fixed except for double fire meteors

--- Tarot/Assets/Scripts/Denis/VFX/ProjectileMove.cs
+++ Tarot/Assets/Scripts/Denis/VFX/ProjectileMove.cs
@@ -26,32 +26,35 @@ public class ProjectileMove : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        speed = 0;
-        ContactPoint contact = collision.contacts[0];
-        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        Vector3 pos = contact.point;
-
-        if(impactPrefab != null)
+        if (collision.gameObject.tag != "met")
         {
-            var impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
-            Destroy(impactVFX, 5);
-        }
+            speed = 0;
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point;
 
-        if(trails.Count > 0)
-        {
-            for(int i = 0; i < trails.Count; i++)
+            if (impactPrefab != null)
             {
-                trails[i].transform.parent = null;
-                var ps = trails[i].GetComponent<ParticleSystem>();
-                if(ps != null)
-                {
-                    ps.Stop();
-                    Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
-                }
+                var impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
+                Destroy(impactVFX, 5);
             }
 
-        }
+            if (trails.Count > 0)
+            {
+                for (int i = 0; i < trails.Count; i++)
+                {
+                    trails[i].transform.parent = null;
+                    var ps = trails[i].GetComponent<ParticleSystem>();
+                    if (ps != null)
+                    {
+                        ps.Stop();
+                        Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                    }
+                }
 
-        Destroy(gameObject);
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
